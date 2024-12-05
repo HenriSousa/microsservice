3fab09d5d1bb30c947db278895e732234f084258
HenriSousa <henrisousa7@hotmail.com>
2024-12-05 15:36:46 -0300

    Update publish
    Configuração para publicar uma Mensagem!

--- Praticando Microsserviços/user/src/main/java/com/ms/user/services/UserService.java
+++ Praticando Microsserviços/user/src/main/java/com/ms/user/services/UserService.java
@@ -5,20 +5,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducers;
 import com.ms.user.repositories.UserRepository;
 
 @Service
 public class UserService {
     
     final UserRepository userRepository;
+    final UserProducers userProducers;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducers userProducers) {
         this.userRepository = userRepository;
+        this.userProducers = userProducers;
     }
 
     @Transactional
     public UserModel save(UserModel userModel) {
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducers.publishMessageEmail(userModel);
+        return userModel;
     }
 
 }
